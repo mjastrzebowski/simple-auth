666117db13e5a383a27a2bf6df26efcf14465a85
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2014-05-13 00:48:38 +0200

    test api server and basic js form handling

--- auth/index.php
+++ auth/index.php
@@ -1,5 +1,44 @@
 <?php
 
-  $user = 'admin';
-  $pass = 'admin123';
+  if (empty($_POST)) {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+  }
+  var_dump($_POST);
 
+  $admin_email = 'admin@admin.com';
+  $admin_password = 'Admin1';
+
+  function validatePassword($val) {
+    if (preg_match("#.*^(?=.{6,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$#", $val)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function validateEmail($val) {
+    if (filter_var($val, FILTER_VALIDATE_EMAIL)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = mysql_real_escape_string(trim($_POST['email']));
+    $password = mysql_real_escape_string(trim($_POST['password']));
+
+    if (validateEmail($email) && validatePassword($password)) {
+      if ($email === $admin_email && $password === $admin_password) {
+        http_response_code(201);
+      } else {
+        http_response_code(401);
+      }
+    } else {
+      http_response_code(400);
+    }
+  } else {
+    http_response_code(500);
+  }
+
+// http_response_code(404);
--- js/app.js
+++ js/app.js
@@ -1,24 +1,66 @@
+// fn based on http://css-tricks.com/snippets/jquery/serialize-form-to-json/
+$.fn.serializeObject = function(){
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+    if (o[this.name]) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
+};
+
+
 (function($) {
+
+  // floated labels from http://codepen.io/soulrider911/pen/ugnyl
   function floatLabel(inputType) {
     $(inputType).each(function() {
       var $this = $(this);
       // on focus add class active to label
       $this.focus(function() {
-        $this.next().addClass("active");
+        $this.next().addClass('active');
       });
       // on blur check field and remove class if needed
       $this.blur(function() {
-        if($this.val() === '' || $this.val() === 'blank') {
+        if ($this.val() === '' || $this.val() === 'blank') {
           $this.next().removeClass();
         }
       });
     });
   }
-  // just add a class of "floatLabel to the input field!"
-  floatLabel(".controls input");
+  floatLabel('.controls input');
+
+
+  // actual form handling
 
   $('form').submit(function(e) {
     e.preventDefault();
-    console.log('test form');
+    console.log($(this).serializeObject());
+    $.ajax({
+      url: $(this).attr('action'),
+      type: $(this).attr('method'),
+      dataType: 'json',
+      data: $(this).serializeObject(),
+      statusCode: {
+        201: function() {
+          alert('logowanie poprawne');
+        },
+        401: function() {
+          alert('niepoprawny login lub haslo');
+        },
+        400: function() {
+          alert('niepoprawne dane');
+        },
+        500: function() {
+          alert('blad uslugi');
+        }
+      }
+    });
   });
 })(jQuery);
